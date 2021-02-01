999c7c5cb77faa55793488db7a358ac9fe320b30
themozel <51420625+themozel@users.noreply.github.com>
2021-02-01 09:13:52 +0100

    Update __init__.py

--- segmentation_models_pytorch/__init__.py
+++ segmentation_models_pytorch/__init__.py
@@ -1,4 +1,4 @@
-from .unet import Unet
+'''from .unet import Unet
 from .unetplusplus import UnetPlusPlus
 from .manet import MAnet
 from .linknet import Linknet
@@ -7,7 +7,7 @@ from .pspnet import PSPNet
 from .deeplabv3 import DeepLabV3, DeepLabV3Plus
 from .pan import PAN
 
-from . import encoders
+from . import encoders'''
 from . import utils
 from . import losses
 
